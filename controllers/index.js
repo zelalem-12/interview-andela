a88e6f7f2d327d455a3fa1dfdf381c52168aa924
zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-02-17 11:23:09 +0300

    mapimng comments to posts

--- controllers/index.js
+++ controllers/index.js
@@ -1,29 +1,30 @@
+const { post } = require("../routes");
 const { getPostsData, getCommentsData, generateCSV } = require("../services");
 
 const postsController = async (req, res) => {
   try {
     const posts = await getPostsData();
-    const csvText = generateCSV(posts, ",");
-    console.log(csvText);
-    res.json(posts);
-  } catch (err) {
-    res
-      .status(err.status || 500)
-      .send(err.message || "AN erro while getting posts data");
-  }
-};
-
-const commentController = async (req, res) => {
-  try {
     const comments = await getCommentsData();
-    const csvText = generateCSV(comments, ",");
+    const mapedPosts = posts.map((post) => {
+      const postComment = (
+        comments.filter((comment) => comment.postId === post.id) || []
+      ).map((comment) => comment.name);
+      const commentedPost = {
+        ...post,
+        comment: postComment.length ? postComment.join(/|/) : postComment,
+      };
+      return commentedPost;
+    });
+    console.log(mapedPosts);
+    const csvText = generateCSV(mapedPosts, ",");
     console.log(csvText);
-    res.json(comments);
+    res.json(mapedPosts);
   } catch (err) {
+    console.log(err);
     res
       .status(err.status || 500)
       .send(err.message || "AN erro while getting posts data");
   }
 };
 
-module.exports = { postsController, commentController };
+module.exports = { postsController };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { postsController, commentController } = require("../controllers");
+const { postsController } = require("../controllers");
 
 /* Get posts  */
 router.get("/posts", postsController);
-/* Get Comments  */
-router.get("/comments", commentController);
 
 module.exports = router;
